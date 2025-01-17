c577d70a43aeee549db39bb40d98297930ad289d
saravanakumar6089 <skvel6089@gmail.com>
2025-01-17 10:44:09 +0530

    test commit

--- src/main/java/com/example/scanteen/OrdersApplication.java
+++ src/main/java/com/example/scanteen/OrdersApplication.java
@@ -2,8 +2,10 @@ package com.example.scanteen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.example.entities","com.example.scanteen"})
+@EnableJpaRepositories(basePackages = "com.example.entities.Repositories")
 public class OrdersApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/scanteen/OrdersController.java
+++ src/main/java/com/example/scanteen/OrdersController.java
@@ -1,15 +1,14 @@
 package com.example.scanteen;
 
-import com.example.entities.entities.OrderStatus;
-import com.example.entities.entities.Orders ;
-import com.example.entities.entities.PaymentStatus;
-import com.example.entities.entities.TransactionType;
-
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
+import com.example.entities.Enums.OrderStatus;
+import com.example.entities.Entities.Orders;
+import com.example.entities.Enums.PaymentStatus;
+import com.example.entities.Enums.TransactionType;
 import com.example.entities.Repositories.OrdersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -44,10 +43,12 @@ public class OrdersController {
             String transactionType = requestBody.get("transaction_type").toString();
 
             // Validate ENUM fields
-            if (!orderStatus.equals("pending") || !paymentStatus.equals("not_paid") ||
-                (!transactionType.equals("cash") && !transactionType.equals("online"))) {
+            if (!OrderStatus.PENDING.name().equalsIgnoreCase(orderStatus) ||
+                !PaymentStatus.NOT_PAID.name().equalsIgnoreCase(paymentStatus) ||
+                (!transactionType.equalsIgnoreCase("cash") && !transactionType.equalsIgnoreCase("online"))) {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", "Invalid ENUM value"));
             }
+
 
             // Create Order Entity
             Orders order = new Orders();
